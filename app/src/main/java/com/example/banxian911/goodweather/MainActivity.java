1358c15d646dd4e952c51e60dda7a2169a321fdf
banxian911 <172647099@qq.com>
2016-01-20 18:52:50 +0800

    测试

--- app/src/main/java/com/example/banxian911/goodweather/MainActivity.java
+++ app/src/main/java/com/example/banxian911/goodweather/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.banxian911.goodweather;
 
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
+//import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
